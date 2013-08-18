2c3c968f3c2147561fee54a5d8b59e6bac1c5658
Lesus <walkingnine@gmail.com>
2013-08-18 08:16:25 +0800

    #001 Fixed the cross platform drawback

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import os
+
 
 setup(name="nose-colorxunit",
-        version = "0.1.1",
+        version = "0.1.2",
         description = "make unittest formatted and colorful output",
+        long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
         license = "APACHE LICENSE VERSION 2.0",
         author = "Lesus",
         author_email = "walkingnine@gmail.com",
-        url="my.oschina.net/swuly302",
+        url="https://github.com/walkingnine/colorunit",
         py_modules = ["colorunit"],
         zip_safe = False,
+        classifiers = [
+            "Development Status :: 2 - Alpha",
+            "License :: APACHE LICENSE VERSION 2.0",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: Implementation :: CPython",
+            ],
         install_requires = [
             "nose>=0.10",
+            "colorama>=0.2.5",
             ],
         entry_points = {
             'nose.plugins.0.10':[
